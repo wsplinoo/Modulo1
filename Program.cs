70cfadaa37ac79a838809326a37fbd28fd3aeb25
wsplinoo <eloisa.lino@edu.senai.br>
2026-08-13 09:41:34 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string nomeProfissional = "João Programador";
+string nomeContato = "joao.prog@exemplo.com";
+int anoNascimento = 2007;
+bool estaDisponivel = false;
+int idade = DateTime.Now.Year - anoNascimento;
+
+if (estaDisponivel == true)
+{
+    Console.WriteLine("--- Cartão de Visitas Digital ---");
+    Console.WriteLine($"Nome: {nomeProfissional}");
+    Console.WriteLine($"Email: {nomeContato}");
+    Console.WriteLine($"Idade: {idade}");
+    Console.WriteLine("Disponível.");
+}
+else
+{
+    Console.WriteLine("--- Cartão de Visitas Digital ---");
+    Console.WriteLine($"Nome: {nomeProfissional}");
+    Console.WriteLine($"Email: {nomeContato}");
+    Console.WriteLine($"Idade: {idade}");
+    Console.WriteLine("Não disponível.");
+}
